ee873215126d55b33cb69c63d03456493e1a4646
Jackson Moffet <jrmoffet@gmail.com>
2018-05-05 14:15:00 -0700

    Can reposition nodes after placement Path auto-updates

--- PathGen.py
+++ PathGen.py
@@ -1,28 +1,27 @@
-from tkinter import *
-
 from math import floor
-
+from tkinter import *
 from Utils import *
 
 
-# TODO Add ability to move control nodes
-# TODO Refresh lines automatically
 # TODO Cascade menu from a control point to select commands to run
 # TODO Load in field overview as background
 # TODO Generate and save robot code to follow the path (how to move the motors between the points)
+# TODO Calculate path for each side of robot (control point == center of drivetrain)
 # TODO Put on github
 
+# NOTE: for now start your path from the start
+
+
 class PathGen(Frame):
-    def __init__(self, master=None, **kw):
-        super().__init__(master, **kw)
+    def __init__(self, master=None):
         self.master = master
         self.window = Canvas(self.master, width=200, height=100)
         self.window.pack(expand=YES, fill=BOTH)
         self.init_window()
         self.nodes = []
-        self.status = None
+        self.node_manip = None
         self.lines = []
-        self.num_samples = 200
+        self.num_samples = 100
 
     def init_window(self):
         self.master.title("GUI")
@@ -40,48 +39,73 @@ class PathGen(Frame):
         edit.add_command(label="Link", command=self.link)
         menu.add_cascade(label="Edit", menu=edit)
 
-        tools = Menu(menu)
-        tools.add_command(label="Generate Paths", command=self.generate_points)
-        menu.add_cascade(label="Tools", menu=tools)
-
-    def on_object_click(self, event):
-        print('Got a click at ({}, {})'.format(event.x, event.y))
-
-    def add_node(self, event):
-        red = "#ff0000"
-        p1, p2, center = ((event.x - 5), (event.y - 5)), ((event.x + 5), (event.y + 5)), (event.x, event.y)
-        if any(dist(a.get_xy(), (event.x, event.y)) < 10 for a in self.nodes):
-            print("HI")
-        else:
-            id = self.window.create_oval(*p1, *p2, fill=red)
-            self.window.tag_bind(id, '<c><Button-1>', self.on_object_click)
-            n = Node(*center, id)
-            self.nodes.append(n)
-
-    def generate_points(self):
+    def generate_path(self):
         points = []
         for i in range(self.num_samples):
             pct = i / (self.num_samples - 1)
             tx = (len(self.nodes) - 1) * pct
             idx = int(tx)
             t = tx - floor(tx)
-            A = self.get_element_clamped(idx - 1)
-            B = self.get_element_clamped(idx)
-            C = self.get_element_clamped(idx + 1)
-            D = self.get_element_clamped(idx + 2)
+
+            A = self.get_node_clamped(idx - 1)
+            B = self.get_node_clamped(idx)
+            C = self.get_node_clamped(idx + 1)
+            D = self.get_node_clamped(idx + 2)
             x = cubic_hermite(A[0], B[0], C[0], D[0], t)
             y = cubic_hermite(A[1], B[1], C[1], D[1], t)
             points.append((x, y))
-        self.remove_lines()
-        for i in range(1, len(points)):
-            self.lines.append(self.window.create_line(*points[i - 1], *points[i], fill="red"))
+        for line in self.lines:
+            self.window.delete(line)
 
-    def get_element_clamped(self, idx):
+        for i in range(len(points) - 1):
+            self.lines.append(self.window.create_line(*points[i], *points[i + 1], fill="red"))
+
+    def get_node_clamped(self, idx):
+        # Later add something so we can adjust the angle of the in/out on the 1st and last nodes
         if idx < 0:
             return self.nodes[0].get_xy()
         if idx >= len(self.nodes):
             return self.nodes[-1].get_xy()
         return self.nodes[idx].get_xy()
+
+    def on_object_click(self, event):
+        self.node_manip = self.closest_node(event)
+        self.window.tag_bind(self.node_manip.tk_id, '<Motion>', self.motion_callback)
+        self.window.tag_bind(self.node_manip.tk_id, '<Button-1>', self.release_node)
+        self.remove_lines()
+
+    def closest_node(self, event):
+        min_dist = dist(self.nodes[0].get_xy(), (event.x, event.y))
+        closest_node = self.nodes[0]
+        for node in self.nodes:
+            if min_dist > dist(node.get_xy(), (event.x, event.y)):
+                min_dist = dist(node.get_xy(), (event.x, event.y))
+                closest_node = node
+        return closest_node
+
+    def motion_callback(self, event):
+        self.window.coords(self.node_manip.tk_id, event.x - 5, event.y - 5, event.x + 5, event.y + 5)
+
+    def release_node(self, event):
+        self.window.tag_unbind(self.node_manip.tk_id, '<Motion>')
+        self.window.tag_bind(self.node_manip.tk_id, '<Button-1>', self.on_object_click)
+        self.node_manip.x = event.x
+        self.node_manip.y = event.y
+        self.node_manip = None
+        self.generate_path()
+
+    def add_node(self, event):
+        red = "#ff0000"
+        p1, p2, center = ((event.x - 5), (event.y - 5)), ((event.x + 5), (event.y + 5)), (event.x, event.y)
+        if any(dist(node.get_xy(), (event.x, event.y)) < 10 for node in self.nodes):
+            return
+        else:
+            tk_id = self.window.create_oval(*p1, *p2, fill=red)
+            self.window.tag_bind(tk_id, '<Button-1>', self.on_object_click)
+            n = Node(*center, tk_id)
+            self.nodes.append(n)
+        if len(self.nodes) > 1:
+            self.generate_path()
 
     def link(self):
         for line in self.lines:
@@ -92,6 +116,11 @@ class PathGen(Frame):
         for i in range(1, len(self.nodes)):
             self.lines.append(self.window.create_line(*self.nodes[i - 1].get_xy(), *self.nodes[i].get_xy(), fill="red"))
 
+    def remove_lines(self):
+        for line in self.lines:
+            self.window.delete(line)
+        self.lines = []
+
     def undo(self):
         if len(self.nodes) > 0:
             self.window.delete(self.nodes[-1].get_id())
@@ -99,22 +128,16 @@ class PathGen(Frame):
         else:
             self.invalid_node_count()
 
+    def invalid_node_count(self):
+        self.node_manip = Label(self.master, text="ERROR: not enough nodes for requested operation")
+        self.node_manip.pack(side=BOTTOM)
+        self.node_manip.after(1000, self.clear_label)
+
     def clear_label(self):
         print("clear label")
-        self.status.pack_forget()
+        self.node_manip.pack_forget()
 
     def app_exit(self):
         print("Exiting")
         exit(0)
 
-    def invalid_node_count(self):
-        self.status = Label(self.master, text="ERROR: not enough nodes for requested operation")
-        self.status.pack(side=BOTTOM)
-        self.status.after(1000, self.clear_label)
-
-    def remove_lines(self):
-        for line in self.lines:
-            self.window.delete(line)
-root = Tk()
-app = PathGen(root)
-root.mainloop()
--- Utils.py
+++ Utils.py
@@ -1,3 +1,15 @@
+def dist(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** (1 / 2)
+
+
+def cubic_hermite(A, B, C, D, t):
+    a = -A / 2 + (3 * B) / 2 - (3 * C) / 2 + D / 2
+    b = A - (5 * B) / 2 + 2 * C - D / 2
+    c = -A / 2 + C / 2
+    d = B
+    return a * (t ** 3) + b * (t ** 2) + c * t + d
+
+
 # This is meant to be a control point/node for computing hermetic splines
 class Node:
     def __init__(self, x=0, y=0, tk_id=0, command=None):
@@ -15,16 +27,3 @@ class Node:
     # Return the tkinter id so we can delete
     def get_id(self):
         return self.tk_id
-
-
-def cubic_hermite(A, B, C, D, t):
-    a = -A / 2.0 + (3.0 * B) / 2.0 - (3.0 * C) / 2.0 + D / 2.0
-    b = A - (5 * B) / 2 + 2 * C - D / 2
-    c = -A / 2 + C / 2
-    d = B
-    return a * t * t * t + b * t * t + c * t + d
-
-
-def dist(a, b):
-    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** (1 / 2)
-
